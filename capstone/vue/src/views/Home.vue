4353c26eef880e5f326ab5289dd434c0fcd22f4d
Angelo Stewart <angelostewart@protonmail.com>
2023-02-01 10:33:04 -0500

    refactor

--- capstone/vue/src/views/Home.vue
+++ capstone/vue/src/views/Home.vue
@@ -3,6 +3,9 @@
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
     <go-to-exercise/>
+     <router-link v-bind:to="{name:'Exercise'}">
+          add exercise 
+      </router-link>
   </div>
 </template>
 

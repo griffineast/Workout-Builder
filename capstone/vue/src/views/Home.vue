cf6f51634e824eb5d13776009dc0eca953faf1d5
Angelo Stewart <angelostewart@protonmail.com>
2023-02-08 12:55:13 -0500

    refactor

--- capstone/vue/src/views/Home.vue
+++ capstone/vue/src/views/Home.vue
@@ -1,24 +1,23 @@
 <template>
   <div class="home">
-    <!-- <display-exercises /> -->
-    <!-- <user-column /> -->
+ 
+    <user-column />
     <workout-cards />
     <create-workout/>
   </div>
 </template>
 
 <script>
-// import UserColumn from "../components/UserColumn.vue";
+import UserColumn from "../components/UserColumn.vue";
 import WorkoutCards from "../components/WorkoutCards.vue";
-// import DisplayExercises from "../components/DisplayExercises.vue";
+
 import CreateWorkout from '../components/CreateWorkout.vue';
 
 export default {
   name: "Home",
   components: {
-    // UserColumn,
+    UserColumn,
     WorkoutCards,
-    // DisplayExercises,
     CreateWorkout,
   },
 };
